355a4c428bc803fa553082a258f6eaf58b774d54
Ariel Rios <ariiel.rah@hotmail.com>
2023-01-31 01:26:04 -0300

    Nuevos cambios

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import logo from '../..src/logo.png'
 
-// import CartWidget from './cartWidget'
-
 function NavBar(props) {
     return (
       <>
@@ -24,9 +22,6 @@ function NavBar(props) {
           <a class="nav-link disabled">Disabled</a>
         </li>
       </ul>
-      <div>
-          <CartWidget/>
-      </div>
       <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
